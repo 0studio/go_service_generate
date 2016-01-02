9c2f4930eff32c1717cd57008458c255df15548e
jixiuf <jixiuf@gmail.com>
2016-01-02 12:42:01 +0800

    generate proxy

--- generator/generator_proxy.go
+++ generator/generator_proxy.go
@@ -6,7 +6,7 @@ func (sd StructDescription) GenerateProxy(property Property, srcDir string) bool
 	if len(pkList) == 1 {
 		return sd.generateProxyPK1(pkList[0], property, srcDir)
 	} else if len(pkList) == 2 {
-		// return sd.generateProxyPK2(pkList[0], pkList[1], property, srcDir)
+		return sd.generateProxyPK2(pkList[0], pkList[1], property, srcDir)
 	}
 	return true
 }
