1eb96098e2a437ab20435d94ce94cd33af92c4a7
jixiuf <jixiuf@gmail.com>
2016-01-04 11:48:16 +0800

    fix tag parser

--- generator/tag.go
+++ generator/tag.go
@@ -1,6 +1,7 @@
 package generator
 
 import (
+	"strconv"
 	"strings"
 )
 
@@ -32,23 +33,94 @@ func (l TagFieldList) GetValue(tagKey string) string {
 
 // parseTag splits a struct field's json tag into its name and
 // comma-separated options.
+// func parseTag(tag string) (tagFields []TagField) {
+// 	for _, tagFieldStr := range strings.Split(tag, ",") {
+// 		i := strings.Index(tagFieldStr, "=")
+// 		if i != -1 {
+// 			tagField := TagField{
+// 				IsKV:     true,
+// 				TagKey:   tagFieldStr[:i],
+// 				TagValue: tagFieldStr[i+1:],
+// 			}
+// 			tagFields = append(tagFields, tagField)
+// 		} else {
+// 			tagField := TagField{
+// 				IsKV:   false,
+// 				TagKey: tagFieldStr,
+// 			}
+// 			tagFields = append(tagFields, tagField)
+
+// 		}
+
+// 	}
+// 	return
+// }
 func parseTag(tag string) (tagFields []TagField) {
-	for _, tagFieldStr := range strings.Split(tag, ",") {
-		i := strings.Index(tagFieldStr, "=")
-		if i != -1 {
+	// When modifying this code, also update the validateStructTag code
+	// in golang.org/x/tools/cmd/vet/structtag.go.
+
+	for tag != "" {
+		// Skip leading space.
+		i := 0
+		for i < len(tag) && tag[i] == ' ' {
+			i++
+		}
+		tag = tag[i:]
+		if tag == "" {
+			break
+		}
+
+		// Scan to colon. A space, a quote or a control character is a syntax error.
+		// Strictly speaking, control chars include the range [0x7f, 0x9f], not just
+		// [0x00, 0x1f], but in practice, we ignore the multi-byte control characters
+		// as it is simpler to inspect the tag's bytes than the tag's runes.
+		i = 0
+		quoteChar := ""
+		for i < len(tag) && tag[i] > ' ' && (tag[i] != ',' || (tag[i] == ',' && quoteChar != "")) && tag[i] != 0x7f {
+			if tag[i] == '"' || tag[i] == '\'' {
+				if i == 0 || (i != 0 && tag[i-1] != '\\') {
+					if quoteChar == "" {
+						quoteChar = tag[i : i+1]
+					} else if quoteChar == tag[i:i+1] {
+						quoteChar = ""
+					}
+				}
+			}
+			i++
+		}
+		if i == 0 || (i < len(tag) && tag[i] != ',') {
+			break
+		}
+
+		j := strings.Index(tag[:i], "=")
+		if j != -1 {
+			unQuoted, err := strconv.Unquote(tag[j+1 : i])
+			if err != nil {
+				unQuoted = tag[j+1 : i]
+				if unQuoted[0] == '\'' && unQuoted[len(unQuoted)-1] == '\'' {
+					unQuoted = unQuoted[1 : len(unQuoted)-1]
+				}
+
+			}
+
 			tagField := TagField{
 				IsKV:     true,
-				TagKey:   tagFieldStr[:i],
-				TagValue: tagFieldStr[i+1:],
+				TagKey:   tag[:j],
+				TagValue: unQuoted,
 			}
 			tagFields = append(tagFields, tagField)
 		} else {
 			tagField := TagField{
 				IsKV:   false,
-				TagKey: tagFieldStr,
+				TagKey: tag[:i],
 			}
 			tagFields = append(tagFields, tagField)
+		}
 
+		if i < len(tag) {
+			tag = tag[i+1:]
+		} else {
+			break
 		}
 
 	}
--- generator/tag_test.go
+++ generator/tag_test.go
@@ -15,3 +15,43 @@ func TestTagParsing(t *testing.T) {
 	assert.Equal(t, "foobar", fields[1].TagKey)
 	assert.Equal(t, "foo", fields[2].TagKey)
 }
+
+func TestTagParsing2(t *testing.T) {
+	fields := parseTag("field,foobar,foo=foofoo")
+	assert.Equal(t, "field", fields[0].TagKey)
+	assert.Equal(t, "foobar", fields[1].TagKey)
+	assert.Equal(t, "foo", fields[2].TagKey)
+	assert.Equal(t, "foofoo", fields[2].TagValue)
+}
+
+func TestTagParsing3(t *testing.T) {
+	fields := parseTag("field,foobar='a,b,c',foo")
+	assert.Equal(t, "field", fields[0].TagKey)
+	assert.Equal(t, "foobar", fields[1].TagKey)
+	assert.Equal(t, "a,b,c", fields[1].TagValue)
+	assert.Equal(t, "foo", fields[2].TagKey)
+}
+
+func TestTagParsing32(t *testing.T) {
+	fields := parseTag("field,foobar=\"'a,b,c'\",foo")
+	assert.Equal(t, "field", fields[0].TagKey)
+	assert.Equal(t, "foobar", fields[1].TagKey)
+	assert.Equal(t, "'a,b,c'", fields[1].TagValue)
+	assert.Equal(t, "foo", fields[2].TagKey)
+}
+
+func TestTagParsing4(t *testing.T) {
+	fields := parseTag("field,foobar=\"a,b,c\",foo=foofoo")
+	assert.Equal(t, "field", fields[0].TagKey)
+	assert.Equal(t, "foobar", fields[1].TagKey)
+	assert.Equal(t, "a,b,c", fields[1].TagValue)
+	assert.Equal(t, "foo", fields[2].TagKey)
+}
+
+func TestTagParsing5(t *testing.T) {
+	fields := parseTag("field,foobar=\"\\\"a,b,c\",foo=foofoo")
+	assert.Equal(t, "field", fields[0].TagKey)
+	assert.Equal(t, "foobar", fields[1].TagKey)
+	assert.Equal(t, "\"a,b,c", fields[1].TagValue)
+	assert.Equal(t, "foo", fields[2].TagKey)
+}
