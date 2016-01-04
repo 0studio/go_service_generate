df7da5787fec163fd145b00aedd118847831d85f
jixiuf <jixiuf@gmail.com>
2016-01-04 14:24:31 +0800

    add clearLocal

--- generator/generate_service.go
+++ generator/generate_service.go
@@ -32,6 +32,9 @@ func (sd StructDescription) GenerateService(property Property, srcDir string) {
 	getAllFunDeclare, getAllImpl := sd.generateServiceGetAll()
 	s = strings.Replace(s, "__GetAllDeclard__", getAllFunDeclare, -1)
 	s = strings.Replace(s, "__GetAllImplments__", getAllImpl, -1)
+	clearLocalDeclare, clearLocalImpl := sd.generateServiceClearLocal()
+	s = strings.Replace(s, "__ClearLocalDeclard__", clearLocalDeclare, -1)
+	s = strings.Replace(s, "__ClearLocalImplments__", clearLocalImpl, -1)
 
 	outputF.WriteString(s)
 
@@ -41,7 +44,7 @@ func (sd StructDescription) generateServiceGetDefault() (declare string, impleme
 	if len(pkList) > 2 {
 		return
 	}
-	declare = fmt.Sprintf("GetDefault(%s, now time.Time) (e %s, ok bool)", sd.GetPKVarDeclear(), sd.StructName)
+	declare = fmt.Sprintf("    GetDefault(%s, now time.Time) (e %s, ok bool)", sd.GetPKVarDeclear(), sd.StructName)
 	implements =
 		fmt.Sprintf(`func (impl *%sServiceImpl) GetDefault(%s, now time.Time) (e %s, ok bool) {
 	e, ok = impl.Get(%s, now)
@@ -58,12 +61,13 @@ func (sd StructDescription) generateServiceGetDefault() (declare string, impleme
 
 	return
 }
+
 func (sd StructDescription) generateServiceGetAll() (declare string, implements string) {
 	pkList := sd.GetPKFieldList()
 	if len(pkList) != 2 {
 		return
 	}
-	declare = fmt.Sprintf("GetAll(%s %s, now time.Time) (eMap %s, ok bool)",
+	declare = fmt.Sprintf("    GetAll(%s %s, now time.Time) (eMap %s, ok bool)",
 		LowerCaseFirstChar(pkList[0].FieldName), pkList[0].FieldGoType, sd.GetSuggestMapName())
 	implements =
 		fmt.Sprintf(`func (impl *%sServiceImpl) GetAll(%s %s, now time.Time) (eMap %s, ok bool) {
@@ -74,6 +78,33 @@ func (sd StructDescription) generateServiceGetAll() (declare string, implements 
 	return impl.MultiGet(%s, idList, now)
 }
 `, sd.StructName, LowerCaseFirstChar(pkList[0].FieldName), pkList[0].FieldGoType, sd.GetSuggestMapName(),
+			LowerCaseFirstChar(pkList[0].FieldName),
+			LowerCaseFirstChar(pkList[0].FieldName))
+
+	return
+}
+
+func (sd StructDescription) generateServiceClearLocal() (declare string, implements string) {
+	pkList := sd.GetPKFieldList()
+	if len(pkList) != 2 {
+		return
+	}
+	declare = fmt.Sprintf("    ClearLocal(%s %s, now time.Time)",
+		LowerCaseFirstChar(pkList[0].FieldName), pkList[0].FieldGoType)
+	implements =
+		fmt.Sprintf(`func (impl *%sServiceImpl) ClearLocal(%s %s, now time.Time) {
+	idList, ok := impl.GetIdListByPK1(%s, now)
+	if !ok {
+		return
+	}
+    if impl.lruStorage != nil {
+		ok = impl.lruStorage.MultiDelete(%s, idList)
+		impl.lruStorage.DeleteIdListByPK1(%s)
+    }
+
+}
+`, sd.StructName, LowerCaseFirstChar(pkList[0].FieldName), pkList[0].FieldGoType,
+			LowerCaseFirstChar(pkList[0].FieldName),
 			LowerCaseFirstChar(pkList[0].FieldName),
 			LowerCaseFirstChar(pkList[0].FieldName))
 
@@ -104,9 +135,10 @@ type __Entity__Service interface {
 	// 本文件是工具生成， 尽量不要编辑此文件，
 	// 当自动生成的文件没法满足你需求的时候
 	// 在另一个文件里实现新的接口， 保持这个文件不变
-	__Entity__ServiceOther
-    __GetDefaultDeclard__
-    __GetAllDeclard__
+    __Entity__ServiceOther
+__GetDefaultDeclard__
+__GetAllDeclard__
+__ClearLocalDeclard__
 
 }
 
@@ -152,7 +184,7 @@ func New__Entity__ServiceCacheAndDB(dt databasetemplate.DatabaseTemplate, log lo
 
 type __Entity__ServiceImpl struct {
 	__Entity__Storage
-	lruStorage   LRUCache__Entity__Storage // only lrucache
+	lruStorage   *LRUCache__Entity__Storage // only lrucache
 	mcDBStorage  __Entity__Storage         // mc and db proxy
 	lruMCStorage __Entity__Storage         // lruCache and mc proxy
     log          logger.Logger
@@ -168,6 +200,7 @@ func (impl *__Entity__ServiceImpl) setOutside(e *__Entity__, now time.Time) {
 }
 __GetDefaultImplments__
 __GetAllImplments__
+__ClearLocalImplments__
 
 `
 )
