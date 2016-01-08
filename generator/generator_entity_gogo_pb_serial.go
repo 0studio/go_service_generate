eebd51e2f51a0122b8ad9e84db3df655b1507542
jixiuf <jixiuf@gmail.com>
2016-01-08 17:05:51 +0800

    尽量 避免 转换

--- generator/generator_entity_gogo_pb_serial.go
+++ generator/generator_entity_gogo_pb_serial.go
@@ -51,7 +51,11 @@ func (sd StructDescription) generateEntitySerial(property Property, srcDir strin
 			}
 
 		} else if fd.IsStringList() {
-			fieldStr += fmt.Sprintf("    pb.%s = %s(e.%s)\n", Camelize(fd.FieldName), fd.getPBInfo().goType, fd.FieldName)
+			if fd.getPBInfo().goType != fd.FieldGoType {
+				fieldStr += fmt.Sprintf("    pb.%s = %s(e.%s)\n", Camelize(fd.FieldName), fd.getPBInfo().goType, fd.FieldName)
+			} else {
+				fieldStr += fmt.Sprintf("    pb.%s = e.%s\n", Camelize(fd.FieldName), fd.FieldName)
+			}
 		} else if fd.IsTime() {
 			fieldStr += fmt.Sprintf("    pb.%s = %s(formatTimeUnix(e.%s))\n", Camelize(fd.FieldName), fd.getPBInfo().goType, fd.FieldName)
 		} else if fd.IsIntList() {
@@ -133,7 +137,12 @@ func (sd StructDescription) generateEntityUnSerial(property Property, srcDir str
 			}
 
 		} else if fd.IsStringList() {
-			fieldStr += fmt.Sprintf("    e.%s = %s(pb.Get%s())\n", fd.FieldName, fd.FieldGoType, Camelize(fd.FieldName))
+			if fd.FieldGoType != fd.getPBInfo().goType {
+				fieldStr += fmt.Sprintf("    e.%s = %s(pb.Get%s())\n", fd.FieldName, fd.FieldGoType, Camelize(fd.FieldName))
+			} else {
+				fieldStr += fmt.Sprintf("    e.%s = pb.Get%s()\n", fd.FieldName, Camelize(fd.FieldName))
+			}
+
 		} else if fd.IsTime() {
 			fieldStr += fmt.Sprintf("    e.%s = newTime(pb.Get%s())\n", fd.FieldName, Camelize(fd.FieldName))
 		} else if fd.IsIntList() {
